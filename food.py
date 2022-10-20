41a0dbef20e2c39cc1e42befc0287800ff0ed284
Nikolay Andreev <bakasaru@list.ru>
2022-10-20 18:11:26 +0300

    feat (02-oop): add constructor parameters

--- food.py
+++ food.py
@@ -1,15 +1,14 @@
 class Food:
-    def __init__(self):
-        self.name = 'Торт'
-        self.taste = 'вкусный'
+    def __init__(self, name, taste):
+        self.name = name
+        self.taste = taste
 
     def get_full_name(self):
         return f'{self.name} ({self.taste})'
 
-cake = Food()
-print(cake.name, cake.taste)
 
-sushi = Food()
-sushi.name = 'Суши'
-sushi.taste = 'вегетарианские'
-print(sushi.name, sushi.taste)
+cake = Food('Торт', 'вкусный')
+print(cake.get_full_name())
+
+sushi = Food('Суши', 'вегетарианские')
+print(sushi.get_full_name())
