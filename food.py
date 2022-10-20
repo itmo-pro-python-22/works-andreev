0401bcac606cadb707abeaf363936be375c73f37
Nikolay Andreev <bakasaru@list.ru>
2022-10-20 18:19:51 +0300

    feat (02-oop): add another example class

--- food.py
+++ food.py
@@ -7,8 +7,21 @@ class Food:
         return f'{self.name} ({self.taste})'
 
 
+class Drink:
+    def __init__(self, name, drink_type, price):
+        self.name = name
+        self.type = drink_type
+        self.price = price
+
+    def get_full_name(self):
+        return f'{self.type} "{self.name}" ({self.price})'
+
+
 cake = Food('Торт', 'вкусный')
 print(cake.get_full_name())
 
 sushi = Food('Суши', 'вегетарианские')
 print(sushi.get_full_name())
+
+latte = Drink('Кофе', 'Латте', 220)
+print(latte.get_full_name())
