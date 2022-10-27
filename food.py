7acf4587fe6da609ed4f387ec4be2abf3cb6e04a
Nikolay Andreev <bakasaru@list.ru>
2022-10-27 18:13:59 +0300

    feat (02-oop): add class field and method for Drink

--- food.py
+++ food.py
@@ -16,15 +16,30 @@ class Food:
     def get_report(cls):
         return f'We have {cls.count} food items'
 
+    @staticmethod
+    def eat():
+        print('Food was eaten')
+
 
 class Drink:
+    count = 0
+
     def __init__(self, name, drink_type, price):
         self.name = name
         self.type = drink_type
         self.price = price
+        self.__class__.count += 1
 
     def __str__(self):
         return f'{self.type} "{self.name}" ({self.price})'
+
+    @classmethod
+    def get_report(cls):
+        return f'We have {cls.count} drinks'
+
+    @staticmethod
+    def drink():
+        print('Drink was drunk')
 
 
 cake_1 = Food('Торт', 'вкусный')
@@ -36,8 +51,15 @@ sushi = Food('Суши', 'вегетарианские')
 print(sushi)
 
 print(Food.get_report())
+Food.eat()
+sushi.eat()
 
 latte = Drink('Латте', 'Кофе', 220)
 print(latte)
+
+kvass = Drink('Натуральный', 'Квас', 150)
+print(kvass)
+
+Drink.drink()
 
 print(sushi == 5)
