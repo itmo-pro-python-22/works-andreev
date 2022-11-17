20c6d5cdfb23188cae5c045c10ebf1fc59e4cdc7
Nikolay Andreev <bakasaru@list.ru>
2022-11-17 14:45:01 +0300

    feat (03-principles): make amount field private with getter and setter

--- food.py
+++ food.py
@@ -4,11 +4,22 @@ class Food:
     def __init__(self, name, taste, amount=1):
         self.name = name
         self.taste = taste
-        self.amount = amount
+        self.__amount = amount
         self.__class__.count += amount
 
+    @property
+    def amount(self):
+        return self.__amount
+
+    @amount.setter
+    def amount(self, value):
+        if value > 0:
+            self.__class__.count += value - self.__amount
+            self.__amount = value
+
+
     def __str__(self):
-        return f'{self.name} ({self.taste})'
+        return f'{self.name} ({self.taste}), {self.__amount} left'
 
     def __eq__(self, other):
         return type(other) == Food and self.name == other.name and self.taste == other.taste
@@ -18,9 +29,9 @@ class Food:
         return f'We have {cls.count} food items'
 
     def consume(self):
-        if self.amount > 0:
+        if self.__amount > 0:
             print(f'{self.name} was eaten')
-            self.amount -= 1
+            self.__amount -= 1
             self.__class__.count -= 1
         else:
             print(f'No {self.name} left')
@@ -54,6 +65,7 @@ print(sushi)
 
 print(Food.get_report())
 sushi.consume()
+sushi.amount += 3
 
 latte = Drink('Латте', 'Кофе', 220)
 print(latte)
@@ -65,3 +77,4 @@ print(sushi == 5)
 
 for item in cake, sushi, latte, kvass:
     item.consume()
+    print(item)
