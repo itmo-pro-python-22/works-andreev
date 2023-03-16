b305c9ff1ae7e1db1a09226bd3028ce8902b7738
Nikolay Andreev <bakasaru@list.ru>
2023-03-16 19:00:56 +0300

    feat (assessment): add dataclasses

--- assessment.py
+++ assessment.py
@@ -4,45 +4,16 @@ from functools import lru_cache
 from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
 class BasePokemon:
-    _name: str
-
-    def __init__(self, name: str) -> None:
-        self._name = name
-
-    def __str__(self) -> str:
-        return f'BasePokemon(name={self._name})'
-
-    @property
-    def name(self):
-        return self._name
-
-    # def get_name(self) -> str:
-    #     return self.__name
+    name: str
 
 
+@dataclass(frozen=True)
 class Pokemon(BasePokemon):
-    __pokemon_id: int
-    __height: int
-    __weight: int
-
-    def __init__(self, pokemon_id: int, name: str, height: int, weight: int):
-        super().__init__(name)
-        self.__pokemon_id = pokemon_id
-        self.__height = height
-        self.__weight = weight
-
-    def __str__(self) -> str:
-        return f'Pokemon(pokemon_id={self.__pokemon_id}, name={self._name}, height={self.__height} decimeters, weight={self.__weight} hectograms)'
-
-    def get_id(self) -> int:
-        return self.__pokemon_id
-
-    def get_height(self) -> int:
-        return self.__height
-
-    def get_weight(self) -> int:
-        return self.__weight
+    pokemon_id: int
+    height: int
+    weight: int
 
 
 class PokeAPI:
